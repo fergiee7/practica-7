07a42f2f3713502de7e195269c71e1f8b94e3fe2
fergiee7 <mmartinez398@alumnos.uaq.mx>
2024-10-05 12:46:03 -0600

    CRUD

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
+// crear estructura
+type User struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
 func main() {
 	router := gin.Default()
+	users := []User{}
+	indexUser := 1
+
 	fmt.Println("Running App")
-	//Tomar archivos
+
+	//Tomar archivos de la carpeta template
 	router.LoadHTMLGlob("templates/*")
 	router.GET("/ping", func(c *gin.Context) { //se define una url
 		c.JSON(http.StatusOK, gin.H{
@@ -18,8 +30,100 @@ func main() {
 		})
 	})
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", gin.H{})
+
+		c.HTML(200, "index.html", gin.H{
+			"title":       "Main website",
+			"total_users": len(users),
+			"users":       users,
+		})
 	})
 
-	router.Run(":8000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	//API URL
+	router.GET("/api/users", func(c *gin.Context) {
+		c.JSON(200, users)
+
+	})
+
+	router.POST("/api/users", func(c *gin.Context) {
+		var user User
+		if c.BindJSON(&user) == nil {
+			user.Id = indexUser
+			users = append(users, user)
+			indexUser++
+			c.JSON(200, user)
+		} else {
+			c.JSON(400, gin.H{
+				"error": "Invalid payload",
+			})
+			return
+		}
+		c.JSON(200, user)
+	})
+
+	//ELIMINAR USUARIO
+	router.DELETE("/api/users/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		idParsed, err := strconv.Atoi(id)
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": "Invalid id",
+			})
+			return
+
+		}
+
+		fmt.Println("Id a borrar: ", id)
+		for i, user := range users {
+			if user.Id == idParsed {
+				users = append(users[:i], users[i+1:]...)
+				c.JSON(200, gin.H{
+					"message": "User deleted",
+				})
+				return
+			}
+		}
+		c.JSON(201, gin.H{})
+
+	})
+
+	//ACTUALIZAR
+	router.PUT("/api/users/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		idParsed, err := strconv.Atoi(id)
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": "Invalid id",
+			})
+			return
+
+		}
+
+		var user User
+		if c.BindJSON(&user) == nil {
+			user.Id = indexUser
+			users = append(users, user)
+			indexUser++
+			c.JSON(200, user)
+		} else {
+			c.JSON(400, gin.H{
+				"error": "Invalid payload",
+			})
+			return
+		}
+
+		fmt.Println("Id a actualizar: ", id)
+		for i, u := range users {
+			if u.Id == idParsed {
+				users[i] = user
+				users[i].Id = idParsed
+				c.JSON(200, users[i])
+				return
+
+			}
+		}
+
+		c.JSON(201, gin.H{})
+	})
+
+	router.Run(":8001") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
