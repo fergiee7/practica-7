1e5de548e97a3d3069e07aa7841781e5949f0ae5
fergiee7 <mmartinez398@alumnos.uaq.mx>
2024-10-05 10:40:55 -0600

    ejemplos HTML

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("WORKS")
+	router := gin.Default()
+	fmt.Println("Running App")
+	//Tomar archivos
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/ping", func(c *gin.Context) { //se define una url
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", gin.H{})
+	})
+
+	router.Run(":8000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
